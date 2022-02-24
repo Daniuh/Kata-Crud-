6bb9c6b6df63aca89a271e3dd563bbf8e8cee466
Daniel Naranjo <88155850+Daniuh@users.noreply.github.com>
2022-02-24 11:50:25 -0500

    Add files via upload

---
+++ demo/src/main/java/co/com/sofka/demo/BackKataApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackKataApplication.class, args);
+	}
+
+}
